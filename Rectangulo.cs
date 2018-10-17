94620be68d76eb5dd6b5ff6c9c4be74bd5ee45ef
Nicolás Gomez <nco_0.sys@hotmail.com>
2018-10-17 04:06:21 -0300

    :tada: plugin

---
+++ Rectangulo.cs
@@ -0,0 +1,25 @@
+﻿using FiguraBase;
+
+namespace Plugins
+{
+    public class Rectangulo : Figura
+    {
+        double alto, ancho;
+
+        public Rectangulo(double alto, double ancho) : base("Rectangulo")
+        {
+            this.alto = alto;
+            this.ancho = ancho;
+        }
+
+        public override double Area()
+        {
+            return alto * ancho;
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * (alto + ancho);
+        }
+    }
+}
